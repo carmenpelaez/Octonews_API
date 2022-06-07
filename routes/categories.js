e8fe843f0dc2ed2471b958cfea91044f3ac6042a
Eduardo Collazo <ecollazodominguez@gmail.com>
2022-06-07 16:34:15 +0200

    Added endpoint getCurrentCategory

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { getCategories } = require("../controllers");
+const { getCategories, getCurrentCategory } = require("../controllers");
 
 const router = Router();
 
 router.get("/categories", getCategories);
+router.get("/categories/:idCategory", getCurrentCategory);
 
 module.exports = router;
